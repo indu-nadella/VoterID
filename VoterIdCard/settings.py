645bd64182768b145889801ea12fd92032b3386d
indunadella <indu.nadella1122@gmail.com>
2025-12-09 08:05:50 +0530

    fix errors

--- VoterIdCard/settings.py
+++ VoterIdCard/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
